3188ab66e0db18afe3e3e52f4083404099b0915b
kandlin <kandlin123@gmail.com>
2025-05-28 16:31:35 +0900

    ㅠㅠㅠㅠㅠㅠ

---
+++ src/main/java/com/example/demo_3/config/RootConfig.java
@@ -0,0 +1,9 @@
+package com.example.demo_3.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RootConfig {
+
+
+}
---
+++ src/main/java/com/example/demo_3/repository/BoardRepository.java
@@ -0,0 +1,20 @@
+package com.example.demo_3.repository;
+
+import com.example.demo_3.domain.Board;
+import com.example.demo_3.repository.search.BoardSearch;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+
+public interface BoardRepository extends JpaRepository<Board, Long>, BoardSearch {
+
+    Page<Board> findByTitleContainingOrderByBnoDesc(String keyword, Pageable pageable);
+
+    @Query("select b from Board b where b.title like concat('%', :keyword,'%') order by b.bno desc ")
+    Page<Board>findByKeyword(String keyword, Pageable pageable);
+
+    @Query(value = "select now()", nativeQuery = true)
+    String getTime();
+}
